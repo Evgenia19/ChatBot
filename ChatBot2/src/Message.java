5a6836d9cf2800bc93dccfcf36e487d2f473503f
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-10-08 00:29:15 +0500

    Add files via upload

---
+++ ChatBot2/src/Message.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Message {
+	final String userId;
+	final long time;
+	final String content;
+	private static Date timer = new Date();
+
+    public Message(String id, String newContent) {
+    	userId = id;
+    	this.time = timer.getTime();
+    	content = newContent;
+  }
+}
