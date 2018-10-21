676fa9e75c42c73d1add93a74bb0a3be6a46c090
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-10-21 20:44:05 +0500

    Add files via upload

--- NewChatBot/src/ChatBot.java
+++ NewChatBot/src/ChatBot.java
@@ -1,15 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChatBot {
 
+	private ChatBotState state;
 	private String userId;
 	private String id;
+	private Map<ChatBotState, String[]> move = new HashMap<ChatBotState, String[]>();
+	private String[] answer;
 	
 	ChatBot(String id) {
 		userId = id;
+		state = ChatBotState.Community;
+		answer = new String[]{"start", "stop", "new game", "end", "more", "help"};
+		move.put(ChatBotState.PlayGame, answer);
 	}
 
 	public User Users(User user) {
-		String msg = ChatBotControls.getMassage(user.content);
-		return new User(userId, msg);
+		if(user.content.hashCode() == "start".hashCode())
+			state = ChatBotState.PlayGame;
+		if(state == ChatBotState.PlayGame) {
+			if(user.content.hashCode() == "end".hashCode())
+				state = ChatBotState.Community;
+			return new User(userId, PlayGame(user.content));
+		}
+		else
+			return new User(userId, Community(user.content));
 	}
 
+	private String PlayGame(String msg) {
+		return ChatBotControls.gameAnswer(msg);
+	}
+	
+	private String Community(String msg) {
+		return ChatBotControls.getMessage(msg);
+	}
 }
