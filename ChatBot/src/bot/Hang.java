4cdd8a19cc4716faa2d249ddd4b6d3832c753fd6
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-11-28 15:57:09 +0500

    Update Hang.java

--- ChatBot/src/bot/Hang.java
+++ ChatBot/src/bot/Hang.java
@@ -1,54 +1,92 @@
 package bot;
+
 import java.util.Random;
 
-public class Hang {
+public class Hang extends Game{
+
+	private Hang hang;
 	private Random random = new Random();
 	private String word;
 	private Character[] answer;
 	private Boolean correct = false;
-	private String[] words = {"Поступок", "Экземпляр", "Чужестранец", "Притяжение"};
+	private Boolean correctSymbol = false;
+	private String[] words = { "поступок", "экземпляр", "чужестранец", "притяжение" };
 	private int lives = 9;
+	private int symbol;
+	public int result = 0;
+	public int game = 0;
 
-	public void hang() {
-		getWord();
-	}
-
-	private void getSymbol(Character symbol) {
+	private void getSymbol(Character smb) {
+		correctSymbol = false;
 		for (int i = 0; i < word.length(); i++) {
-			if (symbol == word.charAt(i)) {
-				answer[i - 1] = symbol;
-			} else {
-				if (lives == 1)
-					correct = true;
-				else
-					lives -= 1;
-			}
+			if (smb == word.charAt(i)) {
+				answer[i] = smb;
+				symbol = symbol - 1;
+				System.out.println(symbol);
+				correctSymbol = true;
+			} 
+		}
+		if(!correctSymbol) {
+			if (lives == 1)
+				correct = true;
+			else
+				lives = lives -  1;
 		}
 	}
 
 	private void getWord() {
 		int numb = random.nextInt(words.length);
 		word = words[numb];
-		for (int i = 0; i < word.length() - 1; i++) {
+		symbol = word.length();
+		answer = new Character[symbol];
+		for (int i = 0; i < symbol; i++) {
 			answer[i] = '_';
 		}
 	}
 
+	public String getText() {
+		String text = "";
+		for (int i = 0; i < answer.length; i++) {
+			text += answer[i] + " ";
+		}
+		return text;
+	}
+
 	public String msgHang() {
-		if (!correct) {
-			String text = "";
-			for (int i = 0; i < answer.length; i++) {
-				text += answer[i];
+		if (correctSymbol)
+			return "You right" + "\n";
+		else
+			return "You wrong" + "\n";
+	}
+
+	public String playHang(Character smb) {
+		getSymbol(smb);
+		if(!correct) {
+			if(symbol == 0) {
+				result += 1;
+				returnEnd();
+				return "You win" + "\n" + getText();
 			}
-			return text;
+			else
+				return "lives:" + lives + "\n" + msgHang() + getText();
 		}
 		else
 			return "You wrong";
 	}
 	
-	public String playHang(Character symbol) {
-		getSymbol(symbol);
-		return msgHang();
+	public void returnEnd() {
+		getGame(game);
+		getResult(result);
+	}
+	
+	@Override
+	public void start() {
+		game += 1;
+		getWord();
 	}
 
+	@Override
+	public String getMsg() {
+		return "lives:" + lives + "\n" + getText();
+	}
 }
