e3e639a5cb1a11a59ae56b9e67817b08a2a55c30
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-11-28 15:56:40 +0500

    Update Main.java

--- ChatBot/src/bot/Main.java
+++ ChatBot/src/bot/Main.java
@@ -1,13 +1,13 @@
 package bot;
 import java.util.Scanner;
 
-public class Main {
 
+public class Main {
 	public static void main(String[] args) {
 		MultiUserBot multiUser = new MultiUserBot();
 		String id = null;
 		Scanner input = new Scanner(System.in);
-		System.out.println("�������: ������� ��� ���");
+		System.out.println("Shaaxter: Please, choose your id");
 		while (true) {
 			try {
 				if (input.hasNext()) {
@@ -15,12 +15,12 @@ public class Main {
 					if (id == null) {
 						if (multiUser.addUser(msg)) {
 							id = msg;
-							System.out.println("\n" + "�������: ��� ����� ������������" + "\n\n" + id + ": ");
+							System.out.println("\n" + "Shaaxter: ok" + "\n\n" + id + ": ");
 						} else
-							System.out.println("\n" + "�������: ������� ������ ���, ����� ��� ����");
+							System.out.println("\n" + "Shaaxter: There is this id, choose different");
 					} else {
 						UserMessage message = multiUser.users(new UserMessage(id, msg, null));
-						System.out.println("\n" + "�������: " + message.content + "\n\n" + id + ": ");
+						System.out.println("\n" + "Shaaxter: " + message.content + "\n\n" + id + ": ");
 					}
 				}
 			} catch (Exception e) {
