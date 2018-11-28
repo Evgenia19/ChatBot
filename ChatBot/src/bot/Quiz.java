ca851870c9d98cbfab070ba45df9d6760632c75f
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-11-28 21:21:09 +0500

    Update Quiz.java

--- ChatBot/src/bot/Quiz.java
+++ ChatBot/src/bot/Quiz.java
@@ -5,25 +5,23 @@ import java.util.Random;
 
 public class Quiz extends Game{
 	
-	private Quiz quiz;
 	private Random random = new Random();
 	private ArrayList<Question> questions;
 	private Question current;
 	public int score = 0;
 	private int questionsLeft;
-	private LoadQuestion loadQuestion;
+	private Load loadQuestion;
 	public int numberQuestions;
 
 	@Override
 	public void start() {
-		loadQuestion = new LoadQuestion();
+		loadQuestion = new Load();
 		questions = loadQuestion.returnQuestions();
 		questionsLeft = questions.size();
 		numberQuestions = questions.size();
 		chooseQuestion();
 	}
 	
-	@Override
 	public String getMsg() {
 		return current.question;
 	}
@@ -32,7 +30,6 @@ public class Quiz extends Game{
         int index = random.nextInt(questions.size());
         current = questions.get(index);
         questions.remove(index);
-        getScore();
     }
 	
 	private boolean isCorrect(String answer) { 
@@ -51,11 +48,4 @@ public class Quiz extends Game{
         chooseQuestion();
         return "Неправильно :с ." + current.question;
     }
-	
-	public void getScore() {
-		getGame(numberQuestions);
-		System.out.println(numberQuestions);
-		getResult(score);
-		System.out.println(score);
-	}
 }
