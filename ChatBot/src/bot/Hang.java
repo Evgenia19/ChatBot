27aa0aef2b0ba119f3f9ce756c876acb0ecf90ed
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-12-07 19:40:31 +0500

    Update Hang.java

--- ChatBot/src/bot/Hang.java
+++ ChatBot/src/bot/Hang.java
@@ -4,90 +4,97 @@ import java.util.ArrayList;
 import java.util.Random;
 
 //TODO Наверное, на логику игры должны быть какие-то тесты?!
-public class Hang extends Game{
+public class Hang extends AbstractGame{
 
-	private Random random = new Random();
-	private Load loadQuestion;
-	private ArrayList<String> words;
-	private String word;
-	//TODO Почему тут Character, а не char?
-	private Character[] answer;
-	//TODO Почему тут Boolean, а не boolean?
-	private Boolean correct = false;
-	//TODO Почему тут Boolean, а не boolean?
-	private Boolean correctSymbol = false;
-	private int lives = 9;
-	private int symbol;
-	public int result = 0;
-	public int game = 0;
+    private Random random = new Random();
+    private Load loadQuestion;
+    private ArrayList<String> words;
+    private String word;
+    private char[] answer;
+    private boolean correct = false;
+    private boolean correctSymbol = false;
+    private int lives = 9;
+    private int symbol;
+    private ArrayList<String> command;
+    public int result = 0;
+    public int game = 0;
 
-	private void getSymbol(Character smb) {
-		correctSymbol = false;
-		for (int i = 0; i < word.length(); i++) {
-			if (smb == word.charAt(i)) {
-				answer[i] = smb;
-				symbol = symbol - 1;
-				System.out.println(symbol);
-				correctSymbol = true;
-			} 
-		}
-		if(!correctSymbol) {
-			if (lives == 1)
-				correct = true;
-			else
-				lives = lives -  1;
-		}
-	}
+    Hang() {
+        command = getCommands();
+        returnCommandsOfGame();
+    }
 
-	private void getWord() {
-		int numb = random.nextInt(words.size());
-		word = words.get(numb);
-		words.remove(numb);
-		symbol = word.length();
-		answer = new Character[symbol];
-		for (int i = 0; i < symbol; i++) {
-			answer[i] = '_';
-		}
-	}
+    private void getSymbol(Character smb) {
+        correctSymbol = false;
+        for (int i = 0; i < word.length(); i++) {
+            if (smb == word.charAt(i)) {
+                answer[i] = smb;
+                symbol = symbol - 1;
+                System.out.println(symbol);
+                correctSymbol = true;
+            }
+        }
+        if(!correctSymbol) {
+            if (lives == 1)
+                correct = true;
+            else
+                lives = lives -  1;
+        }
+    }
 
-	public String getText() {
-		String text = "";
-		for (int i = 0; i < answer.length; i++) {
-			text += answer[i] + " ";
-		}
-		return text;
-	}
+    private void getWord() {
+        int numb = random.nextInt(words.size());
+        word = words.get(numb);
+        words.remove(numb);
+        symbol = word.length();
+        answer = new char[symbol];
+        for (int i = 0; i < symbol; i++) {
+            answer[i] = '_';
+        }
+    }
 
-	public String msgHang() {
-		if (correctSymbol)
-			return "You right" + "\n";
-		else
-			return "You wrong" + "\n";
-	}
+    public String getText() {
+        String text = "";
+        for (int i = 0; i < answer.length; i++) {
+            text += answer[i] + " ";
+        }
+        return text;
+    }
 
-	public String playHang(Character smb) {
-		getSymbol(smb);
-		if(!correct) {
-			if(symbol == 0) {
-				result += 1;
-				return "You win" + "\n" + getText();
-			}
-			else
-				return "lives:" + lives + "\n" + msgHang() + getText();
-		}
-		else
-			return "You wrong";
-	}
-	
-	@Override
-	public void start() {
-		game += 1;
-		loadQuestion = new Load();
-		words = loadQuestion.returnWord();
-		getWord();
-	}
+    public String msgHang() {
+        if (correctSymbol)
+            return "You right" + "\n";
+        else
+            return "You wrong" + "\n";
+    }
 
-	public String getMsg() {
-		return "lives:" + lives + "\n" + getText();
-	}
+    public String playHang(Character smb) {
+        getSymbol(smb);
+        if(!correct) {
+            if(symbol == 0) {
+                result += 1;
+                return "You win" + "\n" + getText();
+            }
+            else
+                return "lives:" + lives + "\n" + msgHang() + getText();
+        }
+        else
+            return "You wrong" + "\n" + word;
+    }
+
+    @Override
+    public void start() {
+        game += 1;
+        loadQuestion = new Load();
+        words = loadQuestion.returnWord();
+        getWord();
+    }
+
+    private void returnCommandsOfGame() {
+        setCommands(command);
+    }
+
+    public String messageForPlayer() {
+        return "lives:" + lives + "\n" + getText();
+    }
 }
