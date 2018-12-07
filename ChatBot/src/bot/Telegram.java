39e91d0f075d19bb3a901c4f398190be3a3ea496
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-12-07 19:45:25 +0500

    Update Telegram.java

--- ChatBot/src/bot/Telegram.java
+++ ChatBot/src/bot/Telegram.java
@@ -12,56 +12,64 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 public class Telegram extends TelegramLongPollingBot{
-	final String token = "759191640:AAGn9dafKkU8LyB__T9myn5upq_jkiQFC0c";
-	final String botUserName = "Chat19Bot";
-	private MultiUserBot multiUser = new MultiUserBot();
+    final String token = "762263340:AAFoaj9m76NXSHuzlvmAuPECW9thZktu86s";
+    final String botUserName = "Chat19Bot";
+    private MultiUserBot multiUser = new MultiUserBot();
 
-	@Override
-	public String getBotUsername() {
-		return botUserName;
-	}
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
 
-	@Override
-	public void onUpdateReceived(Update update) {
-		String msg = update.getMessage().getText();
-		String id = update.getMessage().getChatId().toString();
-		UserMessage message = multiUser.users(new UserMessage(id, msg, null));
-		SendMessage sendMsg = new SendMessage();
+    @Override
+    public void onUpdateReceived(Update update) {
+        String msg = update.getMessage().getText();
+        String id = update.getMessage().getChatId().toString();
+        if(multiUser.addUser(id)) {
+            String c = "";
+        }
+        UserMessage message = multiUser.users(new UserMessage(id, msg));
+        SendMessage sendMsg = new SendMessage();
         sendMsg.setChatId(id);
         sendMsg.setText(message.content);
-        setButtons(message.comand);
+        System.out.println(message.commands);
+        if(message.commands != null) {
+                ReplyKeyboardMarkup keyBoard = setButtons(message.commands);
+                sendMsg.setReplyMarkup(keyBoard);
+        }
         try {
             execute(sendMsg);
         } catch (TelegramApiException e) {
-        	e.printStackTrace();
+            e.printStackTrace();
         }
-		
-	}
 
-	@Override
-	public String getBotToken() {
-		return token;
-	}
-	
-	 public synchronized void setButtons(String[] comands) {
-		 	ArrayList<KeyboardRow> keyboard = new ArrayList<>();
-	        for (String comand: comands) {
-	            KeyboardRow key = new KeyboardRow();
-	            key.add(comand);
-	            keyboard.add(key);
-	        }
-	        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-	        replyKeyboardMarkup.setKeyboard(keyboard);
-	        replyKeyboardMarkup.setOneTimeKeyboard(true);
-	       // return keyboardMarkup;
-	    }
-	 public static void main(String[] args) {
-			ApiContextInitializer.init(); 
-			TelegramBotsApi botApi = new TelegramBotsApi();
-			try {
-			    botApi.registerBot(new Telegram());
-			} catch (TelegramApiException e) {
-			    e.printStackTrace();
-		    }
-	    }
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    public synchronized ReplyKeyboardMarkup setButtons(ArrayList<String> comands) {
+        ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+        for (String comand: comands) {
+            KeyboardRow key = new KeyboardRow();
+            key.add(comand);
+            keyboard.add(key);
+        }
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        return replyKeyboardMarkup;
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botApi = new TelegramBotsApi();
+        try {
+            botApi.registerBot(new Telegram());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
