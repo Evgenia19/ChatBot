39e9d234c4f3dfa11aa5b8c2caf8661ad510aaf5
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-12-07 19:39:05 +0500

    Update Card.java

--- ChatBot/src/bot/Card.java
+++ ChatBot/src/bot/Card.java
@@ -2,16 +2,19 @@ package bot;
 
 
 public class Card {
-	public final Rank rank;
-	public final Suit suit;
-    
-	public Card(Suit suit, Rank rank) {
-		this.suit = suit;
-		this.rank = rank;
-	}
+    public final Rank rank;
+    public final Suit suit;
 
-	//TODO Надо заиспользовать этот метод
-	public static Card pickRandom() {
-		return new Card(Suit.pickRandom(), Rank.pickRandom());
-	}
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public static Card pickRandom() {
+        return new Card(Suit.pickRandom(), Rank.pickRandom());
+    }
+
+    public String strCard() {
+        return suit.toString() + " " + rank.toString();
+    }
 }
