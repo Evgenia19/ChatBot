1d8ea6eee8f254d041225bfb33bc1c37b155b824
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-12-07 19:41:42 +0500

    Update Load.java

--- ChatBot/src/bot/Load.java
+++ ChatBot/src/bot/Load.java
@@ -8,66 +8,66 @@ import java.util.ArrayList;
 
 
 public class Load {
-	private	String fileQuestion = "questions.txt";
-	private	String fileWord = "words.txt";
-	
-	private String load(String fileName) {
+    private	String fileQuestion = "questions.txt";
+    private	String fileWord = "words.txt";
+
+    private String load(String fileName) {
         String content = null;
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(
-                new FileInputStream(fileName), Charset.forName("UTF-8")))) {			
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				content += line + "\n";
-			}
-        } 
+                new FileInputStream(fileName), Charset.forName("UTF-8")))) {
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content += line + "\n";
+            }
+        }
         catch (IOException e) {
-        	//TODO Нужно выбрать другой способ обработки исключений
-        	System.out.println(e.getMessage());
+            //TODO Нужно выбрать другой способ обработки исключений
+            //TODO такой?
+            e.printStackTrace();
         }
         return content;
     }
-	
-	private ArrayList<Question> loadQuestions(String content) {
-		ArrayList<Question> questions = new ArrayList<>();
-		if(content == null)
-			//TODO Не нужно выводить это тут, так как код наверху должен решать выводить что-то куда-то или нет. Сейчас у вас есть консоль, однако, в случаей с UI вам бы понадобилось писать туда
-        	System.out.println("Файл пуст");
+
+    private ArrayList<Question> loadQuestions(String content) {
+        ArrayList<Question> questions = new ArrayList<>();
+        if(content == null) {
+
+        }
         else {
-        	for(String q: content.split("\n\n\n")) {
+            for(String q: content.split("\n\n\n")) {
                 questions.add(makeQuestion(q));
-        	}
+            }
         }
         return questions;
-	}
-	
+    }
+
     private Question makeQuestion(String raw) {
         String[] lines = raw.split("\n\n");
         String text = lines[0];
         if (lines.length != 2)
-        	return new Question(null, null);
+            return new Question(null, null);
         return new Question(text, lines[1]);
     }
-    
-    public ArrayList<Question> returnQuestions() {
-    	String text = load(fileQuestion);
-    	return loadQuestions(text);
-    }
-    
-    private ArrayList<String> loadWords(String content) {
-    	ArrayList<String> words = new ArrayList<String>();
-    	if(content == null)
 
-			//TODO Не нужно выводить это тут, так как код наверху должен решать выводить что-то куда-то или нет. Сейчас у вас есть консоль, однако, в случаей с UI вам бы понадобилось писать туда
-        	System.out.println("Файл пуст");
-        else {
-        	for(String q: content.split("\n"))
-        		words.add(q);
-        }
-    	return words;
+    public ArrayList<Question> returnQuestions() {
+        String text = load(fileQuestion);
+        return loadQuestions(text);
     }
-    
+
+    private ArrayList<String> loadWords(String content) {
+        ArrayList<String> words = new ArrayList<String>();
+        if(content == null) {
+
+        }
+        else {
+            for(String q: content.split("\n"))
+                words.add(q);
+        }
+        return words;
+    }
+
     public ArrayList<String> returnWord() {
-    	String text = load(fileWord);
-    	return loadWords(text);
+        String text = load(fileWord);
+        return loadWords(text);
     }
 }
