6771ead4f11f4c6e74521771aa2e9295d58136db
Evgenia19 <39310317+Evgenia19@users.noreply.github.com>
2018-12-07 21:23:58 +0500

    Update UserMessage.java

--- ChatBot/src/bot/UserMessage.java
+++ ChatBot/src/bot/UserMessage.java
@@ -1,13 +1,19 @@
 package bot;
+
+import java.util.ArrayList;
+
 public class UserMessage {
 
-	final String userId;
-	final String content;
-	final String[] comand;
+    final String userId;
+    final String content;
+    final ArrayList<String> commands;
 
-	public UserMessage(String id, String newContent, String[] comand) {
-		this.userId = id;
-		this.content = newContent;
-		this.comand = comand;
-	}
-}
+    public UserMessage(String id, String newContent, ArrayList<String> commands) {
+        this.userId = id;
+        this.content = newContent;
+        this.commands = commands;
+    }
+
+    public UserMessage(String userId, String content) {
+        this(userId, content, null);
+    }
