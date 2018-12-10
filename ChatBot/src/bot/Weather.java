16b07418397ac2267391397dd4bde8c630de439d
Evgenia19 <evgeniadolgaeva@gmail.com>
2018-12-10 13:21:35 +0500

    //

--- ChatBot/src/bot/Weather.java
+++ ChatBot/src/bot/Weather.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class Weather {
 
     public static String getWeather(String message, Model model) throws IOException {
+        StringBuilder result;
         try(Scanner in = new Scanner((InputStream) new URL("http://api.openweathermap.org/data/2.5/weather?q="
                 + message
                 + "&units=metric&appid=6fff53a641b9b9a799cfd6b079f5cd4e").getContent())) {
